d630da3f067facb68855ff21e9e42a2eecb8bb95
[najib] <najibabiyyu50@gmail.com>
2026-06-11 11:35:02 +0700

    error API postman not connect

--- app/Http/Controllers/ApiBorrowerController.php
+++ app/Http/Controllers/ApiBorrowerController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BorrowerCollection;
+use App\Models\Borrower;
 use Illuminate\Http\Request;
 
 class ApiBorrowerController extends Controller
 {
-    //
+    public function index(){
+        $borrowers = Borrower::all();
+        return new BorrowerCollection($borrowers);
+        }
+        
+    public function show(int $id){
+        $borrowers = Borrower::findOrFail($id);
+        return new BorrowerCollection($borrowers);
+    }
+
+    public function store(Request $request){
+        $borrowers = new Borrower();
+        $borrowers->name = $request->input('name');
+        $borrowers->kontak = $request->input('kontak'); 
+        $borrowers->save();
+
+        return response()->json(
+            ['message'=> 'Data Berhasil Dibuat'], 201
+        );
+    }
+
+    public function update(Request $request, int $id){
+        $borrowers = Borrower::findOrFail($id);
+        $borrowers->name = $request->input('name');
+        $borrowers->kontak = $request->input('kontak');
+        $borrowers->save();
+
+        return response()->json(
+            ['message'=> 'Data Berhasil Diupdate'], 200
+        );
+    }
+
+    public function destroy(int $id){
+        $borrowers = Borrower::findOrFail($id);
+        $borrowers->delete();
+
+        return response()->json(
+            ['message' => 'Data Berhasil Dihapus'], 200
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiBookController;
+use App\Http\Controllers\ApiBorrowerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,10 @@ Route::post('/books', [ApiBookController::class, 'store']);
 Route::get('/books/{$id}', [ApiBookController::class, 'show']);
 Route::put('/books/{$id}', [ApiBookController::class, 'update']);
 Route::delete('/books/{id}', [ApiBookController::class, 'delete']);
+
+// borrowers
+Route::get('/borrower', [ApiBorrowerController::class, 'index']);
+Route::post('/borrower', [ApiBorrowerController::class, 'store']);
+Route::get('/borrower/{$id}', [ApiBorrowerController::class, 'show']);
+Route::put('/borrower/{$id}', [ApiBorrowerController::class, 'update']);
+Route::delete('/borrower/{id}', [ApiBorrowerController::class, 'delete']);
